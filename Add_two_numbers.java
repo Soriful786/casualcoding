05b0545d4794f319174399cddcadd1a00569ab88
Soriful786 <95091430+Soriful786@users.noreply.github.com>
2021-11-26 21:36:21 +0530

    Programme to add two numbers

---
+++ Add_two_numbers.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Add_two_numbers {
+    public static void main(String[]args){
+        //initializing variable a
+        int a =5;
+        //initializing variable b
+        int b=4;
+        System.out.println(a+b);
+    }
+}
